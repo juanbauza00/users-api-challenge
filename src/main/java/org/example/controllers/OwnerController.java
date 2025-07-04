9fee35bac638f39ee33c1bd00ef6df6b1fe0966d
juanbauza00 <juanpablobauza01@gmail.com>
2025-07-04 01:07:09 -0300

    Created controller files

---
+++ src/main/java/org/example/controllers/OwnerController.java
@@ -0,0 +1,9 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/owners")
+public class OwnerController {
+}
